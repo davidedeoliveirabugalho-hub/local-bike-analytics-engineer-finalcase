c791d104706bb7f88d6db465fdb270a4f0ff2c0a
David BUGALHO <davidbugalho@me.com>
2025-11-01 14:43:39 +0000

    feat: update datetime columns in stg_sales__orders

--- models/staging/staging_sales/stg_sales__orders.sql
+++ models/staging/staging_sales/stg_sales__orders.sql
@@ -2,9 +2,9 @@ select
     cast(order_id as integer) as order_id,
     cast(customer_id as integer) as customer_id,
     cast(order_status as integer) as order_status,
-    cast(order_date as date) as order_date,
-    cast(required_date as date) as order_required_date,
-    cast(shipped_date as date) as order_shipped_date,
+    cast(order_date as date) as order_created_at,
+    cast(required_date as date) as order_estimated_delivery_date,
+    cast(shipped_date as date) as order_shipped_at,
     cast(store_id as integer) as store_id,
     cast(staff_id as integer) as staff_id
 from {{ source('sales_local_bike', 'orders') }}
